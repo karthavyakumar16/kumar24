2cefe8ed673bfd821ddce6813c8834bc5d0d9875
Likhita <karthavyachowdary1998@gmail.com>
2024-02-13 11:46:57 +0530

    added

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+        public String name;
+        public String schoolName;
+        public String motherName;
+        public String mobileNo;
+
+        public String getSchoolName() {
+        return schoolName;
+    }
+    }
+
