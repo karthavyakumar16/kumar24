b8a44d62ad7913b7e97f898896c226852a326264
Likhita <karthavyachowdary1998@gmail.com>
2024-02-13 12:25:55 +0530

    123

--- Student.java
+++ Student.java
@@ -4,6 +4,8 @@ public class Student {
         public String schoolName;
         public String motherName;
         public String mobileNo;
+        public String fatherName;
+        public String className;
 
         public String getSchoolName() {
         return schoolName;
